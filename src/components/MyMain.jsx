e8476be15bb45d3b22b995fbdb96fa3a7d7f6bbf
LucaRuggeri5 <luca.ruggeri5@gmail.com>
2025-09-24 20:02:34 +0200

    finalizzato main

--- src/components/MyMain.jsx
+++ src/components/MyMain.jsx
@@ -1,18 +1,25 @@
 const MyMain = () => {
+    const imgList1 = "../src/assets/img/buy-comics-digital-comics.png"
+    const imgList2 = "../src/assets/img/buy-comics-merchandise.png"
+    const imgList3 = "../src/assets/img/buy-comics-subscriptions.png"
+    const imgList4 = "../src/assets/img/buy-comics-shop-locator.png"
+    const imgList5 = "../src/assets/img/buy-dc-power-visa.svg"
     return (
         <main>
-            <div>
-                <span>--Content goes here--</span>
-            </div>
-            <div>
-                <ul>
-                    <li>DIGITAL COMICS</li>
-                    <li>DC MERCHANDISE</li>
-                    <li>SUBSCRIPTION</li>
-                    <li>COMIC SHOP LOCATOR</li>
-                    <li>DC POWER VISA</li>
-                </ul>
-            </div>
+            <section class="sezione-main">
+                <div class="content">
+                    <h1>--Content goes here--</h1>
+                </div>
+                <div class="content-blu">
+                    <ul>
+                        <li><img src={imgList1} alt="" />DIGITAL COMICS</li>
+                        <li><img src={imgList2} alt="" />DC MERCHANDISE</li>
+                        <li><img src={imgList3} alt="" />SUBSCRIPTION</li>
+                        <li><img src={imgList4} alt="" />COMIC SHOP LOCATOR</li>
+                        <li><img src={imgList5} alt="" />DC POWER VISA</li>
+                    </ul>
+                </div>
+            </section>
         </main>
     )
 }
