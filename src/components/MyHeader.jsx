41d2ba3e35038924ebbdc3278510c6804b49b5cd
LucaRuggeri5 <luca.ruggeri5@gmail.com>
2025-09-24 18:50:30 +0200

    finalizzato header

--- src/components/MyHeader.jsx
+++ src/components/MyHeader.jsx
@@ -1,23 +1,30 @@
 const MyHeader = () => {
+    const logoHeader = "../src/assets/img/dc-logo.png"
     return (
         <header>
-            <figure>
-
-            </figure>
-            <nav>
-                <ul>
-                    <li>CHARACTERS</li>
-                    <li>COMICS</li>
-                    <li>MOVIES</li>
-                    <li>TV</li>
-                    <li>GAMES</li>
-                    <li>COLLECTIBLES</li>
-                    <li>VIDEOS</li>
-                    <li>FANS</li>
-                    <li>NEWS</li>
-                    <li>SHOP</li>
-                </ul>
-            </nav>
+            <section class="sezione-header">
+            <div class="container-logo">
+                <figure>
+                    <img src={logoHeader} alt="Logo header" />
+                </figure>
+            </div>
+            <div class="container-link">
+                <nav>
+                    <ul class="">
+                        <li>CHARACTERS</li>
+                        <li>COMICS</li>
+                        <li>MOVIES</li>
+                        <li>TV</li>
+                        <li>GAMES</li>
+                        <li>COLLECTIBLES</li>
+                        <li>VIDEOS</li>
+                        <li>FANS</li>
+                        <li>NEWS</li>
+                        <li>SHOP</li>
+                    </ul>
+                </nav>
+            </div>
+            </section>
         </header>
     )
 }
